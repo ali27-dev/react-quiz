a784f5fbfef553add6b5009538f0eb68e2e69f57
ali27-dev <khanali3229@gmail.com>
2026-01-18 22:11:49 +0500

    Implementing useReducer hook on DataCounter.

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,33 +1,49 @@
-import { useState } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+import { useReducer, useState } from "react";
 
+function reducer(state, action) {
+  console.log(state, action);
+  // return state - action;
+  if (action.type === "dec") return state - action.payload;
+  if (action.type === "inc") return state + action.payload;
+  if (action.type === "setCount") return action.payload;
+  if (action.type === "setStep") return action.payload;
+}
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, setDispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date("jan 18 2026");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    setDispatch({ type: "dec", payload: -1 });
+    // setDispatch(1);
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
+    setDispatch({ type: "inc", payload: 1 });
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    setDispatch({ type: "setCount", payload: Number(e.target.value) });
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
     setStep(Number(e.target.value));
+    // setDispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
+
     setStep(1);
   };
 
