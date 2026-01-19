e73c796530df8c632315cee943998564bfcde284
ali27-dev <khanali3229@gmail.com>
2026-01-19 15:21:45 +0500

    Starting quiz app, First of aal i'm create json server to fatch data from it and after that i'm use useEffect hook and fatch data.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
 function App() {
+  useEffect(function () {
+    async function fakeData(params) {
+      const res = await fetch(`http://localhost:9000/questions`);
+      console.log(res);
+      const data = await res.json();
+      console.log(data);
+    }
+    fakeData();
+  }, []);
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <h1>Hello World</h1>
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
