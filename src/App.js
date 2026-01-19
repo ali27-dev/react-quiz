af2537fbfee8fbc7be4c6d33a50640db733920f8
ali27-dev <khanali3229@gmail.com>
2026-01-19 15:53:49 +0500

    After fatching data from the server then i'm use the useReducer hook  and fatch the data, So that i will be use in my app as soon as possible.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+
+const initailState = {
+  questions: [],
+  status: "Loding",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+    // throw new Error("Action is Unkonwn");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initailState);
+
   useEffect(function () {
+    fetch(`http://localhost:9000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+
+    /*
     async function fakeData(params) {
-      const res = await fetch(`http://localhost:9000/questions`);
-      console.log(res);
-      const data = await res.json();
-      console.log(data);
+      try {
+        const res = await fetch(`http://localhost:9000/questions`);
+        console.log(res);
+        const data = await res.json();
+        dispatch({ type: "dataRecived", payload: data });
+        dispatch({ type: "dataFailed" });
+      } catch (error) {
+        console.error(error);
+      }
     }
-    fakeData();
+    fakeData();*/
   }, []);
   return (
     <div className="app">
